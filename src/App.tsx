3e8f5c5d05139dd47f8c49c9161adc4fca9e1032
TrungVo <thanhtrungvo8401@gmail.com>
2023-10-21 12:45:50 +0700

    Step 12: Config typescript and webpack for .CSS, *.PNG, *.SVG

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+import './styles.css'
+import logo from './mu.png'
+
 function App() {
-  return <div>
+  return <div className='application'>
     <h1>React application - update -1 </h1>
+
+    <img src={logo} alt='React logo' width={100} height={100} />
   </div>
 }
 
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -16,7 +16,19 @@ module.exports = {
             loader: 'babel-loader',
           },
         ],
-      }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline',
+      },
     ]
   },
   output: {
