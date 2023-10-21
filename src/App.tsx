4f609213a2d5e4f3d0b3ea06dbd34277a6d3281b
TrungVo <thanhtrungvo8401@gmail.com>
2023-10-21 16:20:12 +0700

    Step 13: create pro, dev config, merge config using webpack

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import logo from './mu.png'
 
 function App() {
   return <div className='application'>
-    <h1>React application - update -1 </h1>
+    <h1>React application - {process.env.NODE_ENV}</h1>
 
     <img src={logo} alt='React logo' width={100} height={100} />
   </div>
